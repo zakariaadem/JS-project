16f7e459083eb5dc3cabf62e9555096eb9012c32
zakariaadem <92983962+zakariaadem@users.noreply.github.com>
2021-11-08 22:31:55 +0100

    worked with css and js

---
+++ JS project.js
@@ -0,0 +1,9 @@
+function bigImg(x) {
+    x.style.height = "370px";
+    x.style.width = "360px";
+  }
+  
+  function normalImg(x) {
+    x.style.height = "350px";
+    x.style.width = "340px";
+  }
