f88ea7db53ce9932774b8afa9422a9010df9cea9
zakariaadem <92983962+zakariaadem@users.noreply.github.com>
2021-11-09 22:11:20 +0100

    added js function

--- JS project.js
+++ JS project.js
@@ -2,8 +2,14 @@ function bigImg(x) {
     x.style.height = "370px";
     x.style.width = "360px";
   }
-  
-  function normalImg(x) {
+function normalImg(x) {
     x.style.height = "350px";
     x.style.width = "340px";
   }
+
+
+var score_starter = 0
+function get_score(){
+  score_starter = score_starter + 1
+  document.getElementById("score_number").innerHTML = score_starter
+}
