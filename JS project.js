b475487837d3fa08d51b575a92bfb2845a7c4fa2
zakariaadem <zakaria1adam1@gmail.com>
2021-11-12 13:44:23 +0100

    added new functions

--- JS project.js
+++ JS project.js
@@ -20,14 +20,29 @@ function get_score(){
 
 
 var score_50 = 50;
+var price = 50;
+document.getElementById("price").innerHTML = price;
 document.getElementById("img_50").addEventListener("click", function() {
-  if (score > 15){
+  if (score > price){
     score = score + score_50
+    price+= 100
+    document.getElementById("price").innerHTML = price;
   }
 });
 
 
 
+var price_2 = 600;
+var hands = 0;
+document.getElementById("price2").innerHTML = price_2;
+document.getElementById("img_hand").addEventListener("click", function() {
+  if (score > price_2){
+    score++;
+    hands++;
+    price_2 = 1200
+    document.getElementById("price2").innerHTML = price_2;
+  }
+});
 
 
 
@@ -37,9 +52,22 @@ document.getElementById("img_50").addEventListener("click", function() {
 //kjører 10 ganger i sekundet
 setInterval(function(){
   document.getElementById("score_number").innerHTML = score;
-  if (score > 15){
+  if (score > price){
     document.getElementById("img_50").className = "img_502";
   }
-
+  else {
+    document.getElementById("img_50").className = "img_50";
+  }  
 }, 10);
-/*score++;*/
+
+
+setInterval(function() {
+    if (score > price_2){
+      document.getElementById("img_hand").className = "img_hand2";
+    }
+    else {
+      document.getElementById("img_hand").className = "img_hand";
+    }
+  
+      score += hands;
+}, 1000);
