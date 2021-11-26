8a1ad648263777588c6aa643d798ed64d47b816d
zakariaadem <zakaria1adam1@gmail.com>
2021-11-26 14:03:00 +0100

    added random position

--- JS project.js
+++ JS project.js
@@ -12,7 +12,7 @@ function normalImg(x) {
 /* Click_power Function*/
 var name_of_function = " Click Power =  ";
 var score = 0;
-var click_power = 1;
+var click_power = 10000;
 document.getElementById("click_power").innerHTML = name_of_function + click_power;
 function get_score(){
   score = score + click_power;
@@ -80,6 +80,8 @@ document.getElementById("random_golden_cookie").addEventListener("click", functi
 
 
 
+
+
 //kjører 10 ganger i sekundet
 setInterval(function(){
   document.getElementById("score_number").innerHTML = score;
@@ -117,14 +119,67 @@ setInterval(function() {
 
 setInterval(function() {
   var random_number = Math.floor((Math.random() * 100)) + 1;
+  var random_number_for_div2 = Math.floor((Math.random() * 4)) + 1;
+  var random_number_for_div3 = Math.floor((Math.random() * 4)) + 7;
+  var random_number_for_div4 = Math.floor((Math.random() * 4)) + 11;
   if (random_number <= 10){
     document.getElementById("random_golden_cookie").className = "random_golden_cookie2";  
+    document.getElementById("right_div").style.justifyContent = "start";
+    
+    
+    if (random_number_for_div2 == 2) {
+      document.getElementById("right_div").style.justifyContent = "end";
+    } 
+
+    if (random_number_for_div3 == 8) {
+      document.getElementById("right_div").style.justifyContent = "center";
+    } 
+
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     setTimeout(function(){
       document.getElementById("random_golden_cookie").className = "random_golden_cookie";  
-    }, 500);
+    }, 600);
   }
-  
-  console.log(random_number)
+    console.log(random_number)
+    console.log(random_number_for_div2)
+    console.log(random_number_for_div3)
 }, 1000);
 
 
+
+
+/*setInterval(function(){
+  var random_number_for_div = Math.floor((Math.random() * 4)) + 1;
+  if(random_number_for_div == 2){
+    document.getElementById("right_div").style.justifyContent = "center"; 
+  }
+  else{
+    document.getElementById("right_div").style.justifyContent = "unset"; 
+  }
+  console.log(random_number_for_div)
+}, 1500);
+
+
+
+setInterval(function(){
+  var random_number_for_div2 = Math.floor((Math.random() * 4)) + 1;
+  if(random_number_for_div2 == 2){
+    document.getElementById("right_div").style.justifyContent = "end"; 
+  }
+  else{
+    document.getElementById("right_div").style.justifyContent = "unset"; 
+  }
+  console.log(random_number_for_div2)
+}, 2000);*/
