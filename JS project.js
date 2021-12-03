4a54f8a65b42286c158e3b8a52f2db28750c8406
zakariaadem <zakaria1adam1@gmail.com>
2021-12-03 13:28:39 +0100

    added mouse pointer

--- JS project.js
+++ JS project.js
@@ -45,25 +45,20 @@ var name_of_function3 = " (Auto Hand Clicker Function)";
 var price_2 = 1000;
 var hands = 0;
 document.getElementById("price2").innerHTML = price_2 + name_of_function3;
-
-
-
 function generateImage() {
   var img_mouse_pointer = document.createElement('img');
+  img_mouse_pointer.id = "mouse_pointer";
   img_mouse_pointer.src = 'hand_mouse_pointer.png';
   img_mouse_pointer.style.height = "30px";
   img_mouse_pointer.style.width = "30px";
   return img_mouse_pointer;
 }
-
 document.getElementById("img_hand").addEventListener("click", function() {
   if (score > price_2){
     score++;
     hands++;
     price_2+= 4000
-    for (var i = 0; i < 1; i++ ) {
-      document.getElementById('mouse_pointer_div').appendChild(generateImage());
-    }
+    document.getElementById('mouse_pointer_div').appendChild(generateImage());
     document.getElementById("price2").innerHTML = price_2 + name_of_function3;
   }
 });
@@ -85,7 +80,7 @@ document.getElementById("golden_cookie").addEventListener("click", function() {
 });
 
 
-
+/* Random_Golden Cookie Function*/
 var random_golden_cookie_power = 10000;
 document.getElementById("random_golden_cookie").addEventListener("click", function() {
   score = score + random_golden_cookie_power
@@ -96,7 +91,9 @@ document.getElementById("random_golden_cookie").addEventListener("click", functi
 
 
 
-//kjører 10 ganger i sekundet
+//kjører 10 ganger i sekundet//
+
+/* +50 function*/
 setInterval(function(){
   document.getElementById("score_number").innerHTML = score;
   if (score > price){
@@ -108,6 +105,8 @@ setInterval(function(){
 }, 10);
 
 
+
+/* Auto Hand_Clicker Function*/
 setInterval(function() {
     if (score > price_2){
       document.getElementById("img_hand").className = "img_hand2";
@@ -120,6 +119,7 @@ setInterval(function() {
 }, 1000);
 
 
+/* Golden Cookie Function*/
 setInterval(function() {
   if (score > lowest_price && score < highest_price){
     document.getElementById("golden_cookie").className = "golden_cookie2";
@@ -130,13 +130,15 @@ setInterval(function() {
 }, 10);
 
 
+
+/* Random_Golden Cookie Function*/
 setInterval(function() {
   var random_number = Math.floor((Math.random() * 100)) + 1;
   var random_number_for_left = Math.floor((Math.random() * 650)) + 1;
   var random_number_for_top = Math.floor((Math.random() * 700)) + 1;
 
   var the_px = "px";
-  if (random_number <= 10){
+  if (random_number <= 50){
     document.getElementById("random_golden_cookie").className = "random_golden_cookie2";  
     document.getElementById("random_golden_cookie").style.left = random_number_for_left + the_px;
     document.getElementById("random_golden_cookie").style.top = random_number_for_top + the_px;
@@ -144,7 +146,7 @@ setInterval(function() {
 
     setTimeout(function(){
       document.getElementById("random_golden_cookie").className = "random_golden_cookie";  
-    }, 600);
+    }, 1000);
   }
   /*else if (random_number == 2) {
     document.getElementById("random_golden_cookie").className = "random_golden_cookie2";  
